58fb8ab22ae800d714fa80679906113e43297f73
II-SARIP-II <paris.gougne.05@gmail.com>
2026-08-03 12:20:34 +0200

    cporrection for ftp

--- srcs/requirements/bonus/ftp/tools/setup_ftp.sh
+++ srcs/requirements/bonus/ftp/tools/setup_ftp.sh
@@ -1,27 +1,33 @@
 #!/usr/bin/env bash
-
 set -e
 
-WP_PATH="/var/www/html"
-
+# Lecture du secret du mot de passe
 if [ -f /run/secrets/wp_user_password ]; then
     FTP_PASSWORD=$(cat /run/secrets/wp_user_password | tr -d '\r\n')
 else
     FTP_PASSWORD=${FTP_PASSWORD:-"ftppassword"}
 fi
 
-FTP_USER=${FTP_USER:-"pgougne"}
+# Nom de l'utilisateur transmis par docker-compose
+FTP_USER=${FTP_USER:-"user42"}
 
-touch /var/log/vsftpd.log
-chmod 666 /var/log/vsftpd.log
+# Fix PAM Debian pour les conteneurs
+echo "auth required pam_unix.so nullok" > /etc/pam.d/vsftpd
+echo "account required pam_unix.so" >> /etc/pam.d/vsftpd
 
+# Création de l'utilisateur FTP s'il n'existe pas
 if ! id "${FTP_USER}" >/dev/null 2>&1; then
     useradd -m -s /bin/bash "${FTP_USER}"
-    echo "${FTP_USER}:${FTP_PASSWORD}" | chpasswd
     usermod -aG www-data "${FTP_USER}"
-    chown -R www-data:www-data "${WP_PATH}"
 fi
 
+# Mise à jour du mot de passe et whitelist vsftpd
+echo "${FTP_USER}:${FTP_PASSWORD}" | chpasswd
 echo "$FTP_USER" > /etc/vsftpd.userlist
+
+# Configuration des droits du dossier WordPress
+mkdir -p /var/www/html
+chown -R www-data:www-data /var/www/html
+chmod -R 775 /var/www/html
 
 exec vsftpd /etc/vsftpd/vsftpd.conf
